2bfbca6760235c179d26c4baa1a3cad5a199f6f6
Jaomooh <59030221@KMITL.AC.TH>
2017-08-31 14:54:58 +0700

    3 year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -15,6 +15,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 1,I start my programmer life. ");
             Console.WriteLine("Year 2560 semester 2,I learn to Data Base. ");
             Console.WriteLine("Year 2561 semester 1,I learn to Microcontroller ");
+            Console.WriteLine("Year 2561 semester 2,I learn to Opreting systems ");
         }
     }
 }
